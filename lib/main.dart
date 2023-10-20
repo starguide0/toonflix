1404a24237cc2e73e02f396df96a327bca24ba75
starguide <starguide@naver.com>
2023-10-21 00:03:18 +0900

    메인 클래스 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,123 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: Padding(
-          // padding: const EdgeInsets.only(left: 40, right: 40, top: 80), // SizedBox 제거할 경우 이렇게 표현 가능
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const SizedBox(
-                        height: 80,
+      home: const Scaffold(
+          backgroundColor: Color(0xFF181818), body: BodyWidget()),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        // padding: const EdgeInsets.only(left: 40, right: 40, top: 80), // SizedBox 제거할 경우 이렇게 표현 가능
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Hey, selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
                       ),
-                      const Text(
-                        'Hey, selena',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
                       ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+                fontSize: 22,
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              '\$5 194 482',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 44,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(0xFF, 0xF2, 0xB3, 0x3A),
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
